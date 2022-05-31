0c9c5ffdfd071b8dbd4c843030e9ec3476eedd37
achmad <achmadbdrdn@gmail.com>
2022-05-31 17:45:39 +0700

    initial

---
+++ easy/twoSum.js
@@ -0,0 +1,12 @@
+var twoSum = function (nums, target) {
+  let hash = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (target - nums[i] in hash) {
+      return [hash[target - nums[i]], i];
+    } else {
+      hash[nums[i]] = i;
+    }
+  }
+  return [];
+};
