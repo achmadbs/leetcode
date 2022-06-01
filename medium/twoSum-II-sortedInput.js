14d0a725efc99aadc1a4add8c62235684abf1e5b
achmad <achmadbdrdn@gmail.com>
2022-06-01 12:08:19 +0700

    two sum II

---
+++ medium/twoSum-II-sortedInput.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ * @timecomplexity O(log n)
+ */
+var twoSum = function (numbers, target) {
+  let left = 0;
+  let right = numbers.length - 1;
+  while (left <= right) {
+    if (numbers[left] + numbers[right] > target) {
+      right--;
+    } else if (numbers[left] + numbers[right] < target) {
+      left++;
+    } else {
+      return [left + 1, right + 1];
+    }
+  }
+};
